833d09e5d0a8d6b83670dd8f3cc0c7d3c161c525
Matthew Albrecht <matthewwilliamalbrecht@gmail.com>
2024-02-08 09:20:48 -0700

    command palette working

--- src/app/_components/command-menu.tsx
+++ src/app/_components/command-menu.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import {
+  Command,
   CommandDialog,
   CommandEmpty,
   CommandGroup,
@@ -9,9 +10,12 @@ import {
   CommandItem,
   CommandList,
 } from "./ui/command";
+import { useRouter } from "next/navigation";
 
 export function CommandMenu() {
   const [open, setOpen] = useState(false);
+  const [search, setSearch] = useState("");
+  const router = useRouter();
 
   useEffect(() => {
     const down = (e: KeyboardEvent) => {
@@ -26,20 +30,69 @@ export function CommandMenu() {
 
   return (
     <CommandDialog open={open} onOpenChange={setOpen}>
-      <CommandInput placeholder="Type a command or search..." />
-      <CommandList>
-        <CommandEmpty>No results found.</CommandEmpty>
-        <CommandGroup heading="Suggestions">
-          <CommandItem>Calendar</CommandItem>
-          <CommandItem>Search Emoji</CommandItem>
-          <CommandItem>Calculator</CommandItem>
-        </CommandGroup>
-        <CommandGroup heading="Thots">
-          <CommandItem>Kim K</CommandItem>
-          <CommandItem>Obama</CommandItem>
-          <CommandItem>John Wick</CommandItem>
-        </CommandGroup>
-      </CommandList>
+      <Command
+        filter={(value, search) => (value.includes(search) ? 1 : 0)}
+        loop
+      >
+        <CommandInput
+          placeholder="Type a command or search..."
+          value={search}
+          onValueChange={setSearch}
+        />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Pages" forceMount={false}>
+            <CommandItem
+              onSelect={() => {
+                router.push("/");
+                setOpen(false);
+              }}
+            >
+              Home
+            </CommandItem>
+            <CommandItem
+              onSelect={() => {
+                router.push("/work/coachella");
+                setOpen(false);
+              }}
+            >
+              Work
+            </CommandItem>
+            <CommandItem
+              onSelect={() => {
+                router.push("/resume");
+                setOpen(false);
+              }}
+            >
+              Resume
+            </CommandItem>
+            <CommandItem
+              onSelect={() => {
+                router.push("/haiku");
+                setOpen(false);
+              }}
+            >
+              Haiku
+            </CommandItem>
+            <CommandItem
+              onSelect={() => {
+                router.push("/writings");
+                setOpen(false);
+              }}
+            >
+              Writings
+            </CommandItem>
+            <CommandItem
+              onSelect={() => {
+                router.push("/stuff-i-like");
+                setOpen(false);
+              }}
+            >
+              Stuff I Like
+            </CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </Command>
     </CommandDialog>
   );
 }
